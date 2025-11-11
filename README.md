d36eb4b94124a9a5e61aacef181e3e377e9988ff
katherineti <katherineti1609@gmail.com>
2025-11-11 12:55:08 -0400

    feat: Agregado flujo CI/CD estático para GitHub Pages

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 To get started, take a look at src/app/page.tsx.
+
+errores resueltos: https://gemini.google.com/app/3711654daabe28f3 
+
+compilacion
+npm run build
+
+despliegue final
+firebase deploy 
---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Habilita la Exportación Estática, creando una carpeta 'out'
+  output: 'export', 
+};
+
+module.exports = nextConfig;
