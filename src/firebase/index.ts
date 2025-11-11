9b3314c20cf6145e2340cafaef70a2763227e77f
katherineti <katherineti1609@gmail.com>
2025-11-11 07:39:06 -0400

    se resuelve error de .env en el entorno de navegador > 'use client';

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { firebaseConfig } from '@/firebase/config';
@@ -17,11 +18,8 @@ export function initializeFirebase() {
       // Attempt to initialize via Firebase App Hosting environment variables
       firebaseApp = initializeApp();
     } catch (e) {
-      // Only warn in production because it's normal to use the firebaseConfig to initialize
-      // during development
-      if (process.env.NODE_ENV === "production") {
-        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
-      }
+      // Fallback to the local config file if the automatic initialization fails.
+      // This is common during local development.
       firebaseApp = initializeApp(firebaseConfig);
     }
 
